b22770790e8c461e74a0d5cfe6efc47153d3fb78
Carlos Del Valle <cadelvalledieguez@gmail.com>
2021-10-21 08:31:42 +1100

    Implement server authorization

--- app.js
+++ app.js
@@ -1,9 +1,13 @@
 // import  modules
 const express = require("express");
+const { json, urlencoded } = express;
 const mongoose = require("mongoose");
 const morgan = require("morgan");
 const cors = require("cors");
 require("dotenv").config();
+
+const cookieParser = require("cookie-parser");
+const expressValidator = require("express-validator");
 
 // app
 const app = express();
@@ -20,10 +24,17 @@ mongoose
 // middleware
 app.use(morgan("dev"));
 app.use(cors({ origin: true, credentials: true }));
+app.use(json());
+app.use(urlencoded({extended: false}));
+app.use(cookieParser());
+app.use(expressValidator());
+
+
 
 // routes
-const testRoutes = require("./routes/test");
-app.use("/", testRoutes);
+const userRoutes = require("./routes/user");
+app.use("/", userRoutes);
+
 
 // port
 const port = process.env.PORT || 8080;
