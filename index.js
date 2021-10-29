b43f7b8dcb1f254c46da286ed6fad94618b9dfde
Carlos Del Valle <cadelvalledieguez@gmail.com>
2021-10-29 21:29:58 +1100

    Fix cors

--- index.js
+++ index.js
@@ -1,23 +1,35 @@
+const express = require("express")
+const cors = require("cors")
+const app = express()
+
 const todos = require("./routes/todos")
 const signUp = require("./routes/signUp")
 const signIn = require("./routes/signIn")
 
 // set up server (remember that in node.js the use of module is still experimental)
-const express = require("express")
-const app = express()
 const mongoose = require("mongoose")
 require("dotenv").config()
 
-const cors = require("cors")
 
 /* const corsOptions = {
     origin: 'https://sharp-poitras-bece04.netlify.app',
     optionsSuccessStatus: 200 // some legacy browsers (IE11, various SmartTVs) choke on 204
 } */
 
-app.use(cors())
+const whitelist = ['https://sharp-poitras-bece04.netlify.app']
+const corsOptions = {
+    origin: function (origin, callback) {
+        if (whitelist.indexOf(origin) !== -1) {
+            callback(null, true)
+        } else {
+            callback(new Error('Not allowed by CORS'))
+        }
+    }
+}
 
-app.use((req, res, next) => {
+app.use(cors(corsOptions))
+
+/* app.use((req, res, next) => {
     res.header('Access-Control-Allow-Origin', '*');
     res.header('Access-Control-Allow-Headers',
         'Origin, X-Requested-With, Content-Type, Accept, Authorization'
@@ -27,7 +39,7 @@ app.use((req, res, next) => {
         return res.status(200).json({});
     }
     next();
-});
+}); */
 
 //Middlewares (remember everything that uses .use is a middleware)
 app.use(express.json())
