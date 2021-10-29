8063db958f386352b74b49eba7e001149cf9bc59
Carlos Del Valle <cadelvalledieguez@gmail.com>
2021-10-29 22:03:28 +1100

    Fix cors

--- index.js
+++ index.js
@@ -27,8 +27,8 @@ const corsOptions = {
     }
 }
 
-app.use(cors(corsOptions))
 */
+app.use(cors())
 /* app.use((req, res, next) => {
     res.header('Access-Control-Allow-Origin', '*');
     res.header('Access-Control-Allow-Headers',
